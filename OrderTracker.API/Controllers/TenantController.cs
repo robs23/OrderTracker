08dc689de90a2885d13f7a4bd434f90cb020b1ce
robs23 <robert.roszak@gmail.com>
2019-11-09 19:53:36 +0100

    With TenantController

---
+++ OrderTracker.API/Controllers/TenantController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.API.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using OrderTracker.API.Models;
+
+namespace OrderTracker.API.Controllers
+{
+    public class TenantController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ActionResult GetTenant(string token, int id){
+            var tenants = _context.Tenants.Where(x=>x.Id==id && x.Token == token);
+            if(tenants.Any()){
+                return Ok(tenants);
+            }else{
+                return NotFound();
+            }
+        }
+
+        public async Task<ActionResult> CreateTentant(OT_Tenant tenant)
+        {
+            _context.Tenants.Add(tenant);
+            await _context.SaveChangesAsync();
+            return Ok(tenant);
+        }
+    }
+}
