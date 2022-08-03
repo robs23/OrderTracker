91ff943d1d578895d03586b8e00694e3565574c7
Adam Dziuba <mighty.aldenham@gmail.com>
2022-08-03 21:22:07 +0200

    Test change in Employee

--- OrderTracker.API/Models/Employee.cs
+++ OrderTracker.API/Models/Employee.cs
@@ -6,5 +6,11 @@ public class Employee
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    
+    public void Employment()
+    {
+        Employee employee = new Employee();
+        employee.Id = 1;
+
+    }
+
 }
